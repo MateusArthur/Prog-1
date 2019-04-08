228c50c555a367ea593572d2d03e2ba3f4d19828
aluno-uffs <Aluno UFFS>
2019-04-08 15:21:02 -0300

    Aula 8 de Abril

--- Aula 8 de abril/Dicionario.java
+++ Aula 8 de abril/Dicionario.java
@@ -7,6 +7,16 @@ class Dicionario
 		for(int x = 0; x < variaveis.length; x ++)
 			this.variaveis[x] = new Variavel();
 	}
+	public Variavel[] Busca(String n)
+	{
+		for(int i = 0; i < variaveis.length; i++)
+		{
+			if(n.equals(variaveis[i].getNome()))
+			{
+				return variaveis[i];
+			}
+		}
+	}
 	public Variavel[] getVariavel()
 	{
 		return this.variaveis;
